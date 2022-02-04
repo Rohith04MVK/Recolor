41839918dab8d42cfe23e274420e1fc3d8e314b7
Rohith04MVK <007rohitjj04@gmail.com>
2022-02-04 09:08:28 +0530

    :sparkles: Added the resunet

--- recolor/models.py
+++ recolor/models.py
@@ -1,5 +1,8 @@
 import torch
+from fastai.vision.learner import create_body
+from fastai.vision.models.unet import DynamicUnet
 from torch import nn
+from torchvision.models.resnet import resnet18
 
 from .modules import UnetBlock
 
@@ -25,3 +28,10 @@ class Unet(nn.Module):
 
     def forward(self, x):
         return self.model(x)
+
+
+def build_res_unet(n_input=1, n_output=2, size=256):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    body = create_body(resnet18, pretrained=True, n_in=n_input, cut=-2)
+    net_G = DynamicUnet(body, n_output, (size, size)).to(device)
+    return net_G
