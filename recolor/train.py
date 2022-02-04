fc1f2645773a2413c06f05b94672a68a50b0a4d9
Rohith04MVK <007rohitjj04@gmail.com>
2022-02-04 13:04:32 +0530

    :sparkles: Added train function for main and resunet models

---
+++ recolor/train.py
@@ -0,0 +1,37 @@
+import tqdm
+
+from .utils import *
+
+
+def train_model(model, train_dl, val_dl, epochs, display_every=200):
+    data = next(iter(val_dl))  # getting a batch for visualizing the model output after fixed intrvals
+    for e in range(epochs):
+        loss_meter_dict = create_loss_meters()  # function returing a dictionary of objects to
+        i = 0                                  # log the losses of the complete network
+        for data in tqdm(train_dl):
+            model.setup_input(data)
+            model.optimize()
+            update_losses(model, loss_meter_dict, count=data['L'].size(0))  # function updating the log objects
+            i += 1
+            if i % display_every == 0:
+                print(f"\nEpoch {e+1}/{epochs}")
+                print(f"Iteration {i}/{len(train_dl)}")
+                log_results(loss_meter_dict)  # function to print out the losses
+                visualize(model, data, save=False)  # function displaying the model's outputs
+
+
+def pretrain_generator(net_G, train_dl, opt, criterion, epochs, device):
+    for e in range(epochs):
+        loss_meter = AverageMeter()
+        for data in tqdm(train_dl):
+            L, ab = data['L'].to(device), data['ab'].to(device)
+            preds = net_G(L)
+            loss = criterion(preds, ab)
+            opt.zero_grad()
+            loss.backward()
+            opt.step()
+
+            loss_meter.update(loss.item(), L.size(0))
+
+        print(f"Epoch {e + 1}/{epochs}")
+        print(f"L1 Loss: {loss_meter.avg:.5f}")
