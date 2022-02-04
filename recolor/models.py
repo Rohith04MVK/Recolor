55a102239499cafd91ef663739bb19b6c455f8b4
Rohith04MVK <007rohitjj04@gmail.com>
2022-02-04 09:25:39 +0530

    :sparkles: Added PatchDiscriminator

--- recolor/models.py
+++ recolor/models.py
@@ -40,6 +40,27 @@ def build_res_unet(n_input=1, n_output=2, size=256):
     return net_G
 
 
+class PatchDiscriminator(nn.Module):
+    def __init__(self, input_c, num_filters=64, n_down=3):
+        super().__init__()
+        model = [self.get_layers(input_c, num_filters, norm=False)]
+        model += [self.get_layers(num_filters * 2 ** i, num_filters * 2 ** (i + 1), s=1 if i == (n_down-1) else 2) 
+                          for i in range(n_down)] # the 'if' statement is taking care of not using
+                                                  # stride of 2 for the last block in this loop
+        model += [self.get_layers(num_filters * 2 ** n_down, 1, s=1, norm=False, act=False)] # Make sure to not use normalization or
+                                                                                             # activation for the last layer of the model
+        self.model = nn.Sequential(*model)                                                   
+        
+    def get_layers(self, ni, nf, k=4, s=2, p=1, norm=True, act=True): # when needing to make some repeatitive blocks of layers,
+        layers = [nn.Conv2d(ni, nf, k, s, p, bias=not norm)]          # it's always helpful to make a separate method for that purpose
+        if norm: layers += [nn.BatchNorm2d(nf)]
+        if act: layers += [nn.LeakyReLU(0.2, True)]
+        return nn.Sequential(*layers)
+    
+    def forward(self, x):
+        return self.model(x)
+
+
 class MainModel(nn.Module):
     def __init__(self, net_G=None, lr_G=2e-4, lr_D=2e-4,
                  beta1=0.5, beta2=0.999, lambda_L1=100.):
