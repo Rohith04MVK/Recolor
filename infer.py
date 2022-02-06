00a02c27eb07f4f834dca767c45983be4d6e4748
janaSunrise <warriordefenderz@gmail.com>
2022-02-06 20:19:39 +0530

    :sparkles: Improve codebase.

--- infer.py
+++ infer.py
@@ -5,44 +5,61 @@ import PIL
 import torch
 from torchvision import transforms
 
-from models import MainModel, build_res_unet
-from utils import exists, lab_to_rgb, str2bool
+from recolor.models import MainModel, build_res_unet
+from recolor.utils import exists, lab_to_rgb, str2bool
 
 
 def check_opts(opts):
-    exists(f"{opts.checkpoint_dir}/res18-unet.pt", "Resunet is missing or has a differnt name")
-    exists(f"{opts.checkpoint_dir}/final_model_weights.pt", "Final model is missing or has a differnt name")
-
-
+    exists(f"{opts.checkpoint_dir}/res18-unet.pt", "Resunet is missing or has a different name")
+    exists(f"{opts.checkpoint_dir}/final_model_weights.pt", "Final model is missing or has a different name")
 
 
 def build_parser():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--checkpoint-dir", type=str,
-                        dest="checkpoint_dir", help="Path to data",
-                        metavar="IN_PATH", required=False)
+    parser.add_argument(
+        "--checkpoint-dir",
+        type=str,
+        dest="checkpoint_dir",
+        help="Path to data",
+        metavar="IN_PATH",
+        required=False
+    )
 
-    parser.add_argument("--input-image", type=str,
-                        dest="input_path",
-                        help="Path to the image to process",
-                        metavar="INPUT_PATH", required=True)
+    parser.add_argument(
+        "--input-image",
+        type=str,
+        dest="input_path",
+        help="Path to the image to process",
+        metavar="INPUT_PATH",
+        required=True
+    )
 
-    parser.add_argument("--output", type=str,
-                        dest="output_path",
-                        help="Path to save the image",
-                        metavar="OUTPUT_PATH", required=True)
+    parser.add_argument(
+        "--output",
+        type=str,
+        dest="output_path",
+        help="Path to save the image",
+        metavar="OUTPUT_PATH",
+        required=True
+    )
 
-    parser.add_argument("--use-gpu", type=str2bool,
-                        dest="use_gpu",
-                        help="Use a GPU if available",
-                        metavar="USE_GPU", required=True)
+    parser.add_argument(
+        "--use-gpu",
+        type=str2bool,
+        dest="use_gpu",
+        help="Use a GPU if available",
+        metavar="USE_GPU",
+        required=True
+    )
+
     return parser
 
-def main():
 
+if __name__ == "__main__":
     parser = build_parser()
     options = parser.parse_args()
+
     check_opts(options)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
